69fe921a9497666836bc8000c6cccb7a71d4fd1d
nori <nori623p@gmail.com>
2022-02-27 04:06:19 +0100

    add test

--- ex00/test.hpp
+++ ex00/test.hpp
@@ -36,6 +36,33 @@ void ef_n_test(const T &container, size_t n) {
     eftest(container, i);
   }
 }
+template <class T>
+typedef void (*f_append)(typename &T);
 
+// void vector_test() {
+//   std::vector<int> empty;
+//   std::vector<int> v;
+//   std::vector<int> mult_v;
+//   const std::vector<int> &const_empty = empty;
+//   const std::vector<int> &const_v = v;
+//   const std::vector<int> &const_mult_v = mult_v;
+
+//   print_header(__func__);
+//   ft::eftest(empty, 0);
+
+//   v.push_back(0);
+//   ft::eftest(v, 0);
+
+//   push_n_back(mult_v, 3);
+//   ft::ef_n_test(mult_v, 3);
+//   ft::eftest(mult_v, 3);
+
+//   /* const test */
+//   print_header(std::string("const ") + __func__);
+//   ft::eftest(const_empty, 0);
+//   ft::eftest(const_v, 1);
+//   ft::ef_n_test(const_mult_v, 3);
+//   ft::eftest(const_mult_v, 3);
+// }
 }  // namespace ft
 #endif
