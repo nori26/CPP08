0864903fd60c5c2d9126664fadbf0e32da1b2996
nori <nori623p@gmail.com>
2022-02-28 12:58:45 +0100

    add test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -31,11 +31,62 @@ void subject_test() {
   std::stack<int> s(mstack);
 }
 
+void operatoreq_test() {
+  PUT_HEADER();
+  MutantStack<int> mstack;
+  MutantStack<int> mstack2;
+  mstack.push(5);
+  mstack.push(6);
+  mstack.push(7);
+  mstack2 = mstack;
+  for (size_t i = 0; i < 3; i++) {
+    std::cout << mstack2.top() << ", ";
+    mstack2.pop();
+  }
+  std::cout << std::endl;
+}
+
+template <typename Iter>
+void iter_test(Iter begin, Iter end) {
+  for (; begin != end; begin++) {
+    std::cout << *begin << ", ";
+  }
+  std::cout << std::endl;
+}
+
+void it_test(MutantStack<int>::iterator begin, MutantStack<int>::iterator end) {
+  for (; begin != end; begin++) {
+    std::cout << *begin << ", ";
+  }
+  std::cout << std::endl;
+}
+
+void normal_iter_test(MutantStack<int> mstack) {
+  PUT_HEADER();
+  iter_test(mstack.begin(), mstack.end());
+}
+
+void reverse_normal_iter_test(MutantStack<int> mstack) {
+  PUT_HEADER();
+  iter_test(mstack.rbegin(), mstack.rend());
+}
+
+void mstack_init(MutantStack<int> &mstack) {
+  for (int i = 0; i < 10; i++) {
+    mstack.push(i);
+  }
+}
+
+void iterator_test() {
+  MutantStack<int> mstack;
+
+  mstack_init(mstack);
+  normal_iter_test(mstack);
+  reverse_normal_iter_test(mstack);
+}
+
 int main() {
   subject_test();
-  // std::stack<int> stack1;
-  // std::stack<int> stack2;
-  // stack1.push(1);
-  // stack2 = stack1;
-  // std::cout << stack2.top() << std::endl;
+  operatoreq_test();
+  iterator_test();
 }
--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -10,27 +10,21 @@ class MutantStack : public std::stack<T> {
   typedef typename std::stack<T>::container_type container;
   typedef typename container::iterator iterator;
   typedef typename container::reverse_iterator reverse_iterator;
-  typedef typename container::const_iterator const_iterator;
-  typedef typename container::const_reverse_iterator const_reverse_iterator;
   MutantStack(){};
   ~MutantStack(){};
   MutantStack(MutantStack const &other) { *this = other; };
   MutantStack &operator=(MutantStack const &other) {
     {
-      if (this == &other) {
-        return *this;
+      if (this != &other) {
+        std::stack<T>::operator=(other);
       }
-      return std::stack<T>::operator=(other);
+      return *this;
     }
   }
   iterator begin() { return this->c.begin(); };
   iterator end() { return this->c.end(); };
-  const_iterator cbegin() { return this->c.cbegin(); };
-  const_iterator cend() { return this->c.cend(); };
   reverse_iterator rbegin() { return this->c.rbegin(); };
   reverse_iterator rend() { return this->c.rend(); };
-  const_reverse_iterator crbegin() { return this->c.crbegin(); };
-  const_reverse_iterator crend() { return this->c.crend(); };
 };
 
 #endif
