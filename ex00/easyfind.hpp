014cd924becc2c481be72b75f036bf4b42f3a00b
nori <nori623p@gmail.com>
2022-02-28 12:37:05 +0100

    fix const iter

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -4,19 +4,19 @@
 #include <stdexcept>
 namespace ft {
 
-// template <typename T>
-// typename T::iterator easyfind(T &container, int target) {
-//   typedef typename T::iterator Iter;
+template <typename T>
+typename T::iterator easyfind(T &container, int target) {
+  typedef typename T::iterator Iter;
 
-//   Iter begin = container.begin();
-//   Iter end = container.end();
-//   Iter place = std::find(begin, end, target);
+  Iter begin = container.begin();
+  Iter end = container.end();
+  Iter place = std::find(begin, end, target);
 
-//   if (place == end) {
-//     throw std::runtime_error("number not exist in container");
-//   } else
-//     return (place);
-// }
+  if (place == end) {
+    throw std::runtime_error("number not exist in container");
+  } else
+    return (place);
+}
 
 template <class T>
 typename T::const_iterator easyfind(const T &container, int target) {
